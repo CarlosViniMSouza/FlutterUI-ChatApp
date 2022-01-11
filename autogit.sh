c1838b83769b2b332e217d1dc8102cb32533a2a4
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2022-01-11 13:06:14 -0300

    Project Started

--- autogit.sh
+++ autogit.sh
@@ -2,6 +2,6 @@ git init
 git add -A
 git commit -m "Project Started"
 git branch -M master
-git remote add https://github.com/CarlosViniMSouza/FlutterUI-ChatApp.git
+git remote add origin https://github.com/CarlosViniMSouza/FlutterUI-ChatApp.git
 git push -u origin master
 git status
